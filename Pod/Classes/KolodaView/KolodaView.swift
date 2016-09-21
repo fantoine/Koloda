9532f471fb1edf83d5577f681cc1e8fe2146f5ab
Fabien Antoine <fab.antoine@gmail.com>
2016-09-21 14:52:50 +0200

    Custom currentCardIndex counter

--- Pod/Classes/KolodaView/KolodaView.swift
+++ Pod/Classes/KolodaView/KolodaView.swift
@@ -657,7 +657,7 @@ open class KolodaView: UIView, DraggableCardDelegate {
         if !visibleIndexes.isEmpty {
             proceedDeletionInRange(CountableRange(visibleIndexes[0]...visibleIndexes[visibleIndexes.count - 1]))
         }
-        currentCardIndex -= Array(indexRange).filter { $0 < currentCardIndex }.count
+        currentCardIndex += Array(indexRange).filter { $0 < currentCardIndex }.count
         loadMissingCards(missingCardsCount())
         layoutDeck()
         for (index, card) in visibleCards.enumerated() {
